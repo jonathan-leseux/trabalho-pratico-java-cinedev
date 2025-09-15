b55d1dc98ec92a282ee08a374f1f77d4014ff662
Jonathan Leseux e Matheus Pedroso <jonathan.l.costa6@aluno.senai.br e matheus.p.costa13@aluno.senai.br>
2025-09-15 17:08:22 -0300

    Update CineDev.java

--- CineDev.java
+++ CineDev.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 
 public class CineDev {
     static char [][] salaDeCinema = new char [10][20];
+    
     public static void main(String args[]) {
-       
+        Scanner scanner = new Scanner(System.in);
+        inicializarSala();
+
+        int opcao;
     }
 }
